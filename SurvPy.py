7aba3fb279adc068e32e163b37ab2227fce5009a
Thompson-AJ <ajhthompson98@gmail.com>
2022-09-24 14:05:50 -0700

    swap to dms
    swap code to dms version and add a few bits and bobs

--- SurvPy.py
+++ SurvPy.py
@@ -13,49 +13,49 @@ print('Start at reference point, and  ')
 df = pd.read_csv(layerFile)
 points = [list(row) for row in df.values]
 
-for i in range(results - 2):
-    point_1 = points[i]
-    point_2 = points[i+1]
-    angle = atan2(point_1[2] - point_2[2], point_1[1] - point_2[1])
-    angle = degrees(angle)
-    if 0 < angle < 90:             # fix overlapping angles at cardinals
-        angle = 'N ' + str(angle) + ' degrees W'
-    elif 90 <= angle <= 180:
-        angle = 'S ' + str(180 - angle) + ' degrees E'
-    elif 0 >= angle > -90:
-        angle = 'N ' + str(abs(angle)) + ' degrees W'
-    elif -90 >= angle >=-180:
-        angle = 'S ' + str(180 - abs(angle)) + ' degrees W'
-    distance = ((point_1[1] - point_2[1])**2 + (point_1[2] - point_2[2])**2)**0.5
-    print('thence, ' + str(angle) + ' for ' + str(distance) + ' feet.')     # figure out terminology
-
-
-##DMS format
-## -----------------------------
-
-# def dd2dms(dd):
-#     mult = -1 if dd < 0 else 1
-#     mnt,sec = divmod(abs(dd)*3600, 60)
-#     deg,mnt = divmod(mnt, 60)
-#     return mult*deg, mult*mnt, mult*sec
-
-
 # for i in range(results - 2):
 #     point_1 = points[i]
 #     point_2 = points[i+1]
 #     angle = atan2(point_1[2] - point_2[2], point_1[1] - point_2[1])
 #     angle = degrees(angle)
 #     if 0 < angle < 90:             # fix overlapping angles at cardinals
-#         angle_dms = angle
-#         angle = 'N ' + str(dd2dms(angle_dms)) + ' degrees W'
+#         angle = 'N ' + str(angle) + ' degrees W'
 #     elif 90 <= angle <= 180:
-#         angle_dms = 180 - angle
-#         angle = 'S ' + str(dd2dms(angle_dms)) + ' degrees E'
+#         angle = 'S ' + str(180 - angle) + ' degrees E'
 #     elif 0 >= angle > -90:
-#         angle_dms = abs(angle)
-#         angle = 'N ' + str(dd2dms(angle_dms)) + ' degrees W'
+#         angle = 'N ' + str(abs(angle)) + ' degrees W'
 #     elif -90 >= angle >=-180:
-#         angle_dms = 180 - abs(angle)
-#         angle = 'S ' + str(dd2dms(angle_dms)) + ' degrees W'
+#         angle = 'S ' + str(180 - abs(angle)) + ' degrees W'
 #     distance = ((point_1[1] - point_2[1])**2 + (point_1[2] - point_2[2])**2)**0.5
-#     print(str(i) + 'thence, ' + str(angle) + ' for ' + str(distance) + ' feet.')     # figure out terminology
+#     print('thence, ' + str(angle) + ' for ' + str(distance) + ' feet.')     # figure out terminology
+
+
+#DMS format
+# -----------------------------
+
+def dd2dms(dd):
+    mult = -1 if dd < 0 else 1
+    mnt,sec = divmod(abs(dd)*3600, 60)
+    deg,mnt = divmod(mnt, 60)
+    return mult*deg, mult*mnt, round(mult*sec, 2)
+
+
+for i in range(results - 2):
+    point_1 = points[i]
+    point_2 = points[i+1]
+    angle = atan2(point_1[2] - point_2[2], point_1[1] - point_2[1])
+    angle = degrees(angle)
+    if 0 < angle < 90:             # fix overlapping angles at cardinals
+        angle_dms = angle
+        angle = 'N ' + str(dd2dms(angle_dms)) + ' degrees W'
+    elif 90 <= angle <= 180:
+        angle_dms = 180 - angle
+        angle = 'S ' + str(dd2dms(angle_dms)) + ' degrees E'
+    elif 0 >= angle > -90:
+        angle_dms = abs(angle)
+        angle = 'N ' + str(dd2dms(angle_dms)) + ' degrees W'
+    elif -90 >= angle >=-180:
+        angle_dms = 180 - abs(angle)
+        angle = 'S ' + str(dd2dms(angle_dms)) + ' degrees W'
+    distance = ((point_1[1] - point_2[1])**2 + (point_1[2] - point_2[2])**2)**0.5
+    print(str(i) + 'thence, ' + str(angle) + ' for ' + str(distance) + ' feet.')     # figure out terminology
--- prasing.py
+++ prasing.py
@@ -7,11 +7,11 @@ results = len(results) + 1
 print("there are " + str(results) + " lines")
 
 df = pd.read_csv(pointFile)
-points = [list(row) for row in df.values]
+lines = [list(row) for row in df.values]
 
 for i in range(results):
-    direction = points[2]
-    distance = points[3]
+    direction = lines[2]
+    distance = lines[3]
     if 0 < direction > 90:
         direction = 'N ' + str(direction) + ' degrees E'
     elif 00 < direction > 180:
@@ -21,3 +21,6 @@ for i in range(results):
     elif 270 < direction > 360:
         direction = 'N ' + str(360 - direction) + ' degrees W'
     print('thence, ' + str(direction) + ' for ' + str(distance) + ' feet.')     # figure out terminology
+
+
+    #round distance to 2 decimals, round seconds
