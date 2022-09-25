55b6643e1ed755776c62c40156d30dc3dc017562
Thompson-AJ <ajhthompson98@gmail.com>
2022-09-25 11:51:35 -0700

    save point
    commit to save progress

--- SurvPy.py
+++ SurvPy.py
@@ -13,21 +13,21 @@ print('Start at reference point, and  ')
 df = pd.read_csv(layerFile)
 points = [list(row) for row in df.values]
 
-# for i in range(results - 2):
-#     point_1 = points[i]
-#     point_2 = points[i+1]
-#     angle = atan2(point_1[2] - point_2[2], point_1[1] - point_2[1])
-#     angle = degrees(angle)
-#     if 0 < angle < 90:             # fix overlapping angles at cardinals
-#         angle = 'N ' + str(angle) + ' degrees W'
-#     elif 90 <= angle <= 180:
-#         angle = 'S ' + str(180 - angle) + ' degrees E'
-#     elif 0 >= angle > -90:
-#         angle = 'N ' + str(abs(angle)) + ' degrees W'
-#     elif -90 >= angle >=-180:
-#         angle = 'S ' + str(180 - abs(angle)) + ' degrees W'
-#     distance = ((point_1[1] - point_2[1])**2 + (point_1[2] - point_2[2])**2)**0.5
-#     print('thence, ' + str(angle) + ' for ' + str(distance) + ' feet.')     # figure out terminology
+for i in range(results - 2):
+    point_1 = points[i]
+    point_2 = points[i+1]
+    angle = atan2(point_1[2] - point_2[2], point_1[1] - point_2[1])
+    angle = degrees(angle)
+    if 0 < angle < 90:             # fix overlapping angles at cardinals
+        angle = 'N ' + str(angle) + ' degrees W'
+    elif 90 <= angle <= 180:
+        angle = 'S ' + str(180 - angle) + ' degrees E'
+    elif 0 >= angle > -90:
+        angle = 'N ' + str(abs(angle)) + ' degrees W'
+    elif -90 >= angle >=-180:
+        angle = 'S ' + str(180 - abs(angle)) + ' degrees W'
+    distance = ((point_1[1] - point_2[1])**2 + (point_1[2] - point_2[2])**2)**0.5
+    print('thence, ' + str(angle) + ' for ' + str(distance) + ' feet.')     # figure out terminology
 
 
 #DMS format
--- prasing.py
+++ prasing.py
@@ -1,26 +1,50 @@
 import pandas as pd
 
-pointFile = "input.csv"
+lineFile = "ProposedBoundary_COGO_lines.csv"
+f = open("out.txt", "x")
 
-results = pd.read_csv(pointFile)
+results = pd.read_csv(lineFile)
 results = len(results) + 1
 print("there are " + str(results) + " lines")
 
-df = pd.read_csv(pointFile)
+df = pd.read_csv(lineFile)
 lines = [list(row) for row in df.values]
+x = 0
+
+def dd2dms(dd):
+    mult = -1 if dd < 0 else 1
+    mnt,sec = divmod(abs(dd)*3600, 60)
+    deg,mnt = divmod(mnt, 60)
+    return str(round(mult*deg)) + "°" + str(round(mult*mnt)) + "'" + str(round(mult*sec)) + '"' # make sure the rounding is right
 
 for i in range(results):
-    direction = lines[2]
-    distance = lines[3]
-    if 0 < direction > 90:
-        direction = 'N ' + str(direction) + ' degrees E'
-    elif 00 < direction > 180:
-        direction = 'S ' + str(180 - direction) + ' degrees E'
-    elif 180 < direction > 270:
-        direction = 'S ' + str(270 - direction) + ' degrees W'
-    elif 270 < direction > 360:
-        direction = 'N ' + str(360 - direction) + ' degrees W'
-    print('thence, ' + str(direction) + ' for ' + str(distance) + ' feet.')     # figure out terminology
+    line = lines[x]
+    print(x)
+    direction = line[1]
+    distance = line[2]
 
-
-    #round distance to 2 decimals, round seconds
+    if 0 < direction < 90:
+        direction_dms = direction
+        direction = 'North ' + str(dd2dms(direction_dms)) + ' East'
+    elif 90 < direction < 180:
+        direction_dms = 180 - direction
+        direction = 'South ' + str(dd2dms(direction_dms)) + ' East'
+    elif 180 < direction < 270:
+        direction_dms = 270 - direction
+        direction = 'South ' + str(dd2dms(direction_dms)) + ' West'
+    elif 270 < direction < 360:
+        direction_dms = 360 - direction
+        direction = 'North ' + str(dd2dms(direction_dms)) + ' West'
+    elif direction == 0:
+        direction = 'North'
+    elif direction == 90:
+        direction = 'East'
+    elif direction == 180:
+        direction = 'South'
+    elif direction == 270:
+        direction = 'West'
+    f = open("out.txt", "a")
+    f.write('Thence ' + str(direction) + ' a distance of ' + str(round(distance, 2)) + ' feet; ')
+    f.close()
+    x = x + 1
+    print('Thence (' + str(i) + "), " + str(direction) + ' a distance of ' + str(round(distance, 2)) + ' feet; ')     # figure out terminology
