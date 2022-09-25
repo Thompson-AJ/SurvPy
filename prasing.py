38499caa36269c22de225ab8ef8193b37f4ad6f1
Thompson-AJ <ajhthompson98@gmail.com>
2022-09-25 13:39:40 -0700

    Checkpoint
    Checkpoint save after "completing" the script

--- prasing.py
+++ prasing.py
@@ -1,50 +1,73 @@
 import pandas as pd
 
+#read input files
 lineFile = "ProposedBoundary_COGO_lines.csv"
-f = open("out.txt", "x")
+voidFile = "VoidLines.txt"
 
+#create output file
+f = open("out.txt", "w")
+
+#read number of lines in input file for itteration range
 results = pd.read_csv(lineFile)
-results = len(results) + 1
-print("there are " + str(results) + " lines")
+results = len(results)
 
+#read a voidline file to skip certain vector lines
+void = open(voidFile, "r")  #open and read inputfile
+voidLines = void.read() #assign inputfile contents to value
+voidLines = voidLines.split(", ") #create list based on contents and splate using commas
+voidLines = [eval(i) for i in voidLines] #convert list strings to integers
+
+#read the vector file
 df = pd.read_csv(lineFile)
 lines = [list(row) for row in df.values]
-x = 0
 
+#function for converting decimal degrees to degrees minutes seconds
 def dd2dms(dd):
     mult = -1 if dd < 0 else 1
     mnt,sec = divmod(abs(dd)*3600, 60)
     deg,mnt = divmod(mnt, 60)
     return str(round(mult*deg)) + "°" + str(round(mult*mnt)) + "'" + str(round(mult*sec)) + '"' # make sure the rounding is right
 
-for i in range(results):
-    line = lines[x]
-    print(x)
-    direction = line[1]
-    distance = line[2]
+#open the output file
+f = open("out.txt", "a")
 
-    if 0 < direction < 90:
-        direction_dms = direction
-        direction = 'North ' + str(dd2dms(direction_dms)) + ' East'
-    elif 90 < direction < 180:
-        direction_dms = 180 - direction
-        direction = 'South ' + str(dd2dms(direction_dms)) + ' East'
-    elif 180 < direction < 270:
-        direction_dms = 270 - direction
-        direction = 'South ' + str(dd2dms(direction_dms)) + ' West'
-    elif 270 < direction < 360:
-        direction_dms = 360 - direction
-        direction = 'North ' + str(dd2dms(direction_dms)) + ' West'
-    elif direction == 0:
-        direction = 'North'
-    elif direction == 90:
-        direction = 'East'
-    elif direction == 180:
-        direction = 'South'
-    elif direction == 270:
-        direction = 'West'
-    f = open("out.txt", "a")
-    f.write('Thence ' + str(direction) + ' a distance of ' + str(round(distance, 2)) + ' feet; ')
-    f.close()
-    x = x + 1
-    print('Thence (' + str(i) + "), " + str(direction) + ' a distance of ' + str(round(distance, 2)) + ' feet; ')     # figure out terminology
+#loop through lines in input file based on range
+for i in range(results):
+    # check if voiding lines
+    if i in voidLines:
+        f.write("Thence [__];")
+        #print("___")
+    else:
+        #print(i)
+        #print(voidLines)
+        line = lines[i]
+        direction = line[1]
+        distance = line[2]
+        #check the lines for direction quadrant, and call dd2dms function, and 
+        if 0 < direction < 90:
+            direction_dms = direction
+            direction = 'North ' + str(dd2dms(direction_dms)) + ' East'
+        elif 90 < direction < 180:
+            direction_dms = 180 - direction
+            direction = 'South ' + str(dd2dms(direction_dms)) + ' East'
+        elif 180 < direction < 270:
+            direction_dms = 270 - direction
+            direction = 'South ' + str(dd2dms(direction_dms)) + ' West'
+        elif 270 < direction < 360:
+            direction_dms = 360 - direction
+            direction = 'North ' + str(dd2dms(direction_dms)) + ' West'
+        elif direction == 0:
+            direction = 'North'
+        elif direction == 90:
+            direction = 'East'
+        elif direction == 180:
+            direction = 'South'
+        elif direction == 270:
+            direction = 'West'
+        #write the vector line info
+        #f.write('Thence ' + str(direction) + ' a distance of ' + str(round(distance, 2)) + ' feet; ') #this is for final
+        f.write('Thence (' + str(i + 1) + "), " + str(direction) + ' a distance of ' + str(round(distance, 2)) + ' feet; ') #this is for working
+        print('Thence (' + str(i + 1) + "), " + str(direction) + ' a distance of ' + str(round(distance, 2)) + ' feet; ')
+
+#close the output file
+f.close()
