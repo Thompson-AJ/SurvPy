24f549041d06ce63012f491d8c4bfee4763dc3d6
Thompson-AJ <ajhthompson98@gmail.com>
2022-10-07 15:16:53 -0700

    Update LegalDescription.py
    Removed need for editlines file, added functionality for land border, changed variables, added beginning point, other fixes.

--- LegalDescription.py
+++ LegalDescription.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 #read input files
 lineFile = "ProposedBoundary_COGO.csv"
-editFile = "editLines.txt"
+#editFile = "editLines.txt"
 
 #create/overwrite output files
 f = open("out.txt", "w") 
@@ -16,12 +16,6 @@ e = open("cardinals.csv", "w") #this file is for use in arcpro
 #read number of lines in input file for itteration range
 results = pd.read_csv(lineFile)
 results = len(results)
-
-#read a voidline file to skip certain vector lines
-edit = open(editFile, "r")  #open and read inputfile
-editLines = edit.read() #assign inputfile contents to value
-editLines = editLines.split(", ") #create list based on contents and splate using commas
-editLines = [eval(i) for i in editLines] #convert list strings to integers
 
 #read the vector file
 df = pd.read_csv(lineFile)
@@ -37,6 +31,11 @@ def dd2dms(dd):
 #open the output file
 f = open("out.txt", "a")
 e = open("cardinals.csv", "a") #use in arcpro
+
+startPoint = "Beginning at a point on the Northwest corner of the Northeast quarter of the Northeast quarter of Section 11; "
+print(startPoint)
+f.write(startPoint)
+
 #loop through lines in input file based on range
 for i in range(results):
     #print(i)
@@ -44,23 +43,24 @@ for i in range(results):
     line = lines[i]
     direction = line[1]
     distance = line[2]
+    landBorder = line[3]
     #check the lines for direction quadrant, and call dd2dms function, and 
     if 0 < direction < 90:
         direction_dms = direction
         direction = 'North ' + str(dd2dms(direction_dms)) + ' East'
-        arcdirection = 'N ' + str(dd2dms(direction_dms)) + ' E'
+        arcDirection = 'N ' + str(dd2dms(direction_dms)) + ' E'
     elif 90 < direction < 180:
         direction_dms = 180 - direction
         direction = 'South ' + str(dd2dms(direction_dms)) + ' East'
-        arcdirection = 'S ' + str(dd2dms(direction_dms)) + ' E'
+        arcDirection = 'S ' + str(dd2dms(direction_dms)) + ' E'
     elif 180 < direction < 270:
         direction_dms = 90 - (270 - direction) #inverting angle
         direction = 'South ' + str(dd2dms(direction_dms)) + ' West'
-        arcdirection = 'S ' + str(dd2dms(direction_dms)) + ' W'
+        arcDirection = 'S ' + str(dd2dms(direction_dms)) + ' W'
     elif 270 < direction < 360:
         direction_dms = 360 - direction
         direction = 'North ' + str(dd2dms(direction_dms)) + ' West'
-        arcdirection = 'N ' + str(dd2dms(direction_dms)) + ' W'
+        arcDirection = 'N ' + str(dd2dms(direction_dms)) + ' W'
     elif direction == 0:
         direction = 'Northerly'
     elif direction == 90:
@@ -70,15 +70,16 @@ for i in range(results):
     elif direction == 270:
         direction = 'Westerly'
     #write the vector line info
-    writeBearing = ('Thence ' + str(direction) + ' a distance of ' + str(round(distance, 2)) + ' feet; ')
+    writeBearing = ('Thence ' + str(direction) + ' a distance of ' + str(round(distance, 2)) + ' feet')
     writeBearingplusnum = ('Thence (' + str(i + 1) + "), " + str(direction) + ' a distance of ' + str(round(distance, 2)) + ' feet')
-    if i in editLines:
-        f.write(writeBearingplusnum + " along the boundary of the _ ; ")
-        print(writeBearingplusnum + " along the boundary of the _ ; ")
+    
+    if landBorder != "0":
+        f.write(writeBearing + " along the boundary of the " + str(landBorder) + "; ")  #change writebearing variable
+        print(writeBearing + " along the boundary of the " + str(landBorder) + "; ")
     else:
         f.write(writeBearing + "; ")
         print(writeBearing + "; ")
-    e.write(arcdirection + ", ") #use in arcpro
+    e.write(arcDirection + ", ") #use in arcpro
 
 #close the output file. This script was created by A.J. Thompson
 f.close()
